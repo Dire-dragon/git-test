5096b279143bfa8d825f82f04e484dfc25c36f08
Maktub <Mak_tub@163.com>
2021-09-13 15:57:40 +0800

    first commit

---
+++ src/main/java/com/maktub/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.maktub.git;
+
+/**
+ * @author Ddr
+ * @create 2021-09-13 15:53
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
